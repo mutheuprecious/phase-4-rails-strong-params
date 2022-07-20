a092704021625d86901f0065a07bbc3077638a04
Precious <preciousmaregwa1997@gmail.com>
2022-07-20 10:38:56 +0300

    commit tree 34a0c47a4b2bf345ca67e4dd60803277251614dc

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,25 +1,31 @@
 class BirdsController < ApplicationController
 
-  # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
-  end
-
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
-
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
+    # GET /birds
+    def index
+      birds = Bird.all
+      render json: birds
     end
+  
+    # POST /birds
+    def create
+      bird = Bird.create(bird_params)
+      render json: bird, status: :created
+    end
+  
+    # GET /birds/:id
+    def show
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        render json: bird
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+      end
+    end
+  
+    private
+  
+    def bird_params
+      params.permit(:name,:species)
+    end
+  
   end
-
-end
